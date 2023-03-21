aad18c31375b5b79e3b868a359b53df3c00ca93e
IrinaStyazhkina <irastyazhkina@gmail.com>
2023-03-21 22:14:29 +0300

    fix auth

--- rangiffler-client/src/api/authClient.ts
+++ rangiffler-client/src/api/authClient.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { CLIENT, SECRET } from "./config";
+import {Buffer} from "buffer";
 
 export const authClient = axios.create({
-    baseURL: process.env.REACT_APP_AUTH_URL,
+    baseURL: "http:127.0.0.1:9000",
     headers: {
         "Content-type": "application/json",
         "Authorization": `Basic ${Buffer.from(`${CLIENT}:${SECRET}`).toString("base64")}`,
