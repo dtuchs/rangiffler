321c93fa83052a23231cd74cac55f30014e37bc2
IrinaStyazhkina <irastyazhkina@gmail.com>
2023-03-22 15:04:28 +0300

    add token

--- rangiffler-client/src/api/apiClient.ts
+++ rangiffler-client/src/api/apiClient.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 import {GATEWAY_URL} from "./config";
 
+const token = sessionStorage.getItem("id_token");
+
 export const apiClient = axios.create({
   baseURL: GATEWAY_URL,
   withCredentials: true,
   headers: {
     "Accept": "application/json",
     "Content-type": "application/json",
+    'Authorization': `Bearer ${token}`,
   }
 });
