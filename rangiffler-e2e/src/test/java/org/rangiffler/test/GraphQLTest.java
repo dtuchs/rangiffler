81c4b195f3f1920d42e1c957d5a8236d60ec3c78
dtuchs <dtuchs@gmail.com>
2024-09-28 11:55:42 +0300

    added apollo deps

--- rangiffler-e2e/src/test/java/org/rangiffler/test/GraphQLTest.java
+++ rangiffler-e2e/src/test/java/org/rangiffler/test/GraphQLTest.java
@@ -1,9 +1,24 @@
 package org.rangiffler.test;
 
+import com.apollographql.java.client.ApolloClient;
+import io.qameta.allure.okhttp3.AllureOkHttp3;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GraphQLTest {
+
+  private final OkHttpClient httpClient = new OkHttpClient.Builder()
+      .addNetworkInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+      .addNetworkInterceptor(new AllureOkHttp3())
+      .build();
+
+  private final ApolloClient apolloClient = new ApolloClient.Builder()
+      .serverUrl("http://127.0.0.1:8080/graphql")
+      .callFactory((Call.Factory) httpClient)
+      .build();
 
   @Test
   void firstTest() {
