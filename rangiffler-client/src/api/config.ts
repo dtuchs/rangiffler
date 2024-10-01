1d09578f98f783c035cc2122e7aae5794115588f
dtuchs <dtuchs@gmail.com>
2024-10-01 17:34:07 +0300

    remove ext files

--- rangiffler-client/src/api/config.ts
+++
@@ -1,5 +0,0 @@
-export const FRONT_URL = process.env.REACT_APP_FRONT_URL;
-export const GATEWAY_URL = process.env.REACT_APP_GATEWAY_URL;
-export const AUTH_URL = process.env.REACT_APP_AUTH_URL;
-export const CLIENT = "client";
-export const SECRET = "secret";
